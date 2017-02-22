bface0bbad0d26540a050da5af1bd7e812d1a83e
LordWader <wadikarsmith@gmail.com>
2017-02-22 23:37:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var users = require('./models/users');
+var app = express();
+
+app.set('view engine','pug');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/static',express.static('public'));
+
+app.get('/', function(req,res){
+  res.render('index', { title: "ROZGAAR" });
+})
+
+app.post('/show', function(req,res){
+  var username = req.body.name;
+  console.log('Bomb has been planted!');
+
+})
+
+mongoose.connect('mongodb://localhost/rozgaar',function(err){
+  if (err) console.log(err);
+  else console.log('Connected to DB!');
+});
+
+app.listen(3000,function(err){
+  if (err) console.log(err);
+  else console.log('Server is listening!');
+});
---
+++ models/users.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WorkerSchema = new Schema({
+  jobcard_no: Number,
+  family_id: Number,
+  adhar_no: Array,
+  mobile: Array,
+  name: Array,
+
+})
+
+var Worker = mongoose.model('Worker', WorkerSchema);
+
+module.exports = mongoose.model('Worker', WorkerSchema);

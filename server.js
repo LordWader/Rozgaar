9f86d5d7b9ea2efca0e6fd2d25edecc4fca82897
Susmit Wadikar <wadikarsmith@gmail.com>
2017-03-05 01:27:17 +0530

    hardcoded PORT

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ mongoose.connect(dbURL,function(err){
   else console.log('Connected to DB!');
 });
 
-
-app.set('port', (3000));
 app.set('view engine','pug');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({ secret: '123456789', resave: false, saveUninitialized: true}));
@@ -52,7 +50,7 @@ app.post('/searchUser', function(req,res){
   res.render('searchUser', { title: "OZGAAR" });
 })
 
-app.listen(3000,function(err){
+app.listen((process.env.PORT || 5000),function(err){
   if (err) console.log(err);
   else console.log('Server is listening!');
 });
